88df579892038b1c1ae3161c75330dc450876256
wtjerry <wtjerry123@gmail.com>
2017-04-05 19:14:02 +0200

    added DriveView UserControl and CommonRunParameters UserControl to TestDrive UI

--- TestDrive/Form1.Designer.cs
+++ TestDrive/Form1.Designer.cs
@@ -32,19 +32,46 @@ namespace TestDrive
         private void InitializeComponent()
         {
             this.mainMenu1 = new System.Windows.Forms.MainMenu();
-            this.Menu = this.mainMenu1;
-            this.components = new System.ComponentModel.Container();
+            this.driveView = new RobotView.DriveView();
+            this.commonRunParameters = new RobotView.CommonRunParameters();
+            this.SuspendLayout();
+            // 
+            // driveView
+            // 
+            this.driveView.Drive = null;
+            this.driveView.Location = new System.Drawing.Point(3, 90);
+            this.driveView.Name = "driveView";
+            this.driveView.Size = new System.Drawing.Size(292, 289);
+            this.driveView.TabIndex = 0;
+            // 
+            // commonRunParameters
+            // 
+            this.commonRunParameters.Acceleration = 0.3F;
+            this.commonRunParameters.Location = new System.Drawing.Point(3, 0);
+            this.commonRunParameters.Name = "commonRunParameters";
+            this.commonRunParameters.Size = new System.Drawing.Size(360, 84);
+            this.commonRunParameters.Speed = 0.5F;
+            this.commonRunParameters.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.Text = "Form1";
             this.AutoScroll = true;
-
-            this.driveView = new DriveView();
+            this.ClientSize = new System.Drawing.Size(638, 455);
             this.Controls.Add(this.driveView);
+            this.Controls.Add(this.commonRunParameters);
+            this.Menu = this.mainMenu1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
 
         private DriveView driveView;
+        private CommonRunParameters commonRunParameters;
     }
 }
 
